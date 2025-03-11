623f2bcc32e9f3d5591a4538c82d97d6901b1e21
Sabatino Panella <sabatinopanella@hotmail.it>
2025-03-11 19:41:18 +0100

    Constant semi-final fixes.

--- src/main/java/it/unibo/javajump/view/ScaleUtils.java
+++ src/main/java/it/unibo/javajump/view/ScaleUtils.java
@@ -2,9 +2,9 @@ package it.unibo.javajump.view;
 
 import java.awt.*;
 
-import static it.unibo.javajump.utility.Constants.MAINVIEWCENTERDIV;
-import static it.unibo.javajump.utility.Constants.MAINVIEWDRAWXINIT;
-import static it.unibo.javajump.utility.Constants.MAINVIEWDRAWYINIT;
+import static it.unibo.javajump.utility.Constants.MAIN_VIEW_CENTER_DIV;
+import static it.unibo.javajump.utility.Constants.MAIN_VIEW_DRAW_X_INIT;
+import static it.unibo.javajump.utility.Constants.MAIN_VIEW_DRAW_Y_INIT;
 
 public class ScaleUtils {
     public static Rectangle computeScaledRectangle(int virtualWidth, int virtualHeight, Dimension panelSize) {
@@ -15,13 +15,13 @@ public class ScaleUtils {
         if (panelAspect > targetAspect) {
             drawHeight = panelSize.height;
             drawWidth = Math.round(drawHeight * targetAspect);
-            drawX = (panelSize.width - drawWidth) / MAINVIEWCENTERDIV;
-            drawY = MAINVIEWDRAWYINIT;
+            drawX = (panelSize.width - drawWidth) / MAIN_VIEW_CENTER_DIV;
+            drawY = MAIN_VIEW_DRAW_Y_INIT;
         } else {
             drawWidth = panelSize.width;
             drawHeight = Math.round(drawWidth / targetAspect);
-            drawX = MAINVIEWDRAWXINIT;
-            drawY = (panelSize.height - drawHeight) / MAINVIEWCENTERDIV;
+            drawX = MAIN_VIEW_DRAW_X_INIT;
+            drawY = (panelSize.height - drawHeight) / MAIN_VIEW_CENTER_DIV;
         }
         return new Rectangle(drawX, drawY, drawWidth, drawHeight);
     }
